1e7370de3327e9339170d602c38dadea734026a0
Rich Downie <rich@richdownie.com>
2011-03-30 11:31:23 -0400

    fixed

--- features/step_definitions/custom/header_footer_steps.rb
+++ features/step_definitions/custom/header_footer_steps.rb
@@ -1,14 +1,7 @@
 Given /I check for the header and footer/ do
-  #header
   assert(@browser.div(:class, /links/).exists?)
-  # text = "Logout"
-  assert(@browser.contains_text("Logout"))
-  # text = "Support"
-  assert(@browser.contains_text("Support"))
-  # text = "Help"
-  assert(@browser.contains_text("Help"))
-  assert(@browser.image(:src, /logo/).exists?)
-  #footer
+  text = ["Logout", "Support", "Help"]
+  text.collect { |text| assert(@browser.text) }
   assert(@browser.div(:class, /footer/).exists?)
-  assert(@browser.image(:src, /footer_rs_logo-black.png/).exists?)
+  assert(@browser.image(:src, /logo.png/).exists?)
 end
